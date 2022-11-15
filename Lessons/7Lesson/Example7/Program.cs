d7340082ef37b13f5097fc10e8d144ee019cee54
GeekBrains-Vladimir <114289281+GeekBrains-Vladimir@users.noreply.github.com>
2022-11-15 21:19:51 +0500

    add 7 lesson

---
+++ Lessons/7Lesson/Example7/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Обход папки*/
+
+
+void CatalogInfo(string path, string indent = "")
+{
+    DirectoryInfo catalogs = new DirectoryInfo(path);
+    foreach (var currentCatalog in catalogs.GetDirectories())
+    {
+        Console.WriteLine($"{indent}{currentCatalog.Name}");
+        CatalogInfo(currentCatalog.FullName, indent + " ");
+    }
+    foreach (var item in catalogs.GetFiles())
+    {
+        Console.WriteLine($"{indent}{item.Name}");
+    }
+}
+string path = @"K:\GeekBrains\11 Знакомство с языками программирования\2Introduction to programming languages\Lessons\1Lesson\Example001_HelloConsole";
+CatalogInfo(path);
